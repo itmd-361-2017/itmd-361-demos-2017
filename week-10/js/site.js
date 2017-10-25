1f2eca0ebf878795ba18dd12780da89f07df5609
Karl Stolley <karl.stolley@gmail.com>
2017-10-25 16:15:29 -0500

    Add example self-invoking function; linted

--- week-10/js/site.js
+++ week-10/js/site.js
@@ -1,1 +1,15 @@
-console.log("JavaScript!");
+(function(){
+  var double = multiplier(2);
+  var triple = multiplier(3);
+
+  console.log(double(14)); // 28
+  console.log(triple(7)); // 21
+  console.log(typeof(multiplier)) // ?
+
+  function multiplier(factor) {
+    return function(number) {
+      return number * factor;
+    }
+  }
+  
+})();
