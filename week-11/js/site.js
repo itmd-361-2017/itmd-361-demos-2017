30f8859d7e46fe5c2bccb1f28a71a94e95dc5334
Karl Stolley <karl.stolley@gmail.com>
2017-10-31 15:23:08 -0500

    Add explanatory comments to jQuery use

--- week-11/js/site.js
+++ week-11/js/site.js
@@ -1,5 +1,8 @@
+// Release the dollar-sign from jQuery's control
 $.noConflict();
 
+// Re-introduce dollar sign scoped to self-executing
+// function
 (function($) {
   $('#masthead').html('OMG jQuery');
   $('html').removeClass('no-js').addClass('js');
