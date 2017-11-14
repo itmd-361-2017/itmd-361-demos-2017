e23e3da6d08ed6d50b69e5cca816bec0f9710375
Karl Stolley <karl.stolley@gmail.com>
2017-11-13 22:40:07 -0600

    JavaScript to handle form data and hit GitHub API

--- week-13/js/site.js
+++ week-13/js/site.js
@@ -4,5 +4,21 @@ $.noConflict();
 // Re-introduce dollar sign scoped to self-executing
 // function
 (function($) {
-  
+  $('#username-form').append('<ul id="results">');
+  $('#username-form').on('submit', function(e) {
+    var username = $('#username').val();
+    var url = 'https://api.github.com/users/' + username
+    e.preventDefault();
+    console.log("Form submitted! Let's get the info for", username);
+    $.get(url, function(data) {
+      //console.log("YOUR DATA:", data);
+      $('#results').append(
+        '<li>'
+        + '<h2>' + data.name + '</h2>'
+        + '<img src="' + data.avatar_url + '" />'
+        + data.location
+        + '</li>'
+      );
+    })
+  });
 })(jQuery);
